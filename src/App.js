b47662147d5be44c929fa07a211ede180f25c8a5
Gaurav Negi <gauravnegi2235@gmail.com>
2024-08-24 16:35:43 +0530

    created list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from  'react';
 
-function App() {
+const App = ()=>{
+  const [currentData, setValue] = useState('');
+  const [data , storeElement] = useState([]);
+  function storeValue(){
+    // setValue(value);
+    // storeElement([...data , currentData])
+    storeElement((data)=>{
+      let updatedData = [...data , currentData];
+      setValue('');
+      return updatedData;
+    })
+  }
+  function showTextData(e){
+    setValue(e.target.value);
+    
+  }
+  function delText(i){
+      const updatedData = data.filter((ele,id)=>{
+        return id!== i;
+      })
+      storeElement(updatedData);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <h1>WELCOME TO TODOLIST</h1>
+      <div className="card-container">
+        <input type="text" 
+        className="input-text"
+         placeholder=
+        "Enter Text"
+         value = {currentData} 
+         onChange={showTextData}>
+        </input>
+        <button className="add-button" onClick={storeValue}>ADD</button>
+        {data.length > 0 && (
+          <ul>
+            {data.map((item, index) => (
+              <li key={index}>{item}
+              <button className='del-button' onClick={()=>{
+                delText(index)
+              }}>DEL</button>
+              </li>
+              
+            ))}
+          </ul>
+        )}
+      </div>
 
+
+    </>
+  )
+}
 export default App;
